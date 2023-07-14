11b85f5e3c5785373114bee5de10badcd15a92a3
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-07-14 17:46:47 -0400

    A1

--- src/questionary.js
+++ src/questionary.js
@@ -8,32 +8,55 @@ function Survey() {
   const questions = QaA;
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [isOver, setIsOver] = useState(false);
+ const [score, setScore] = useState(0);
 
   const handleAnswerChange = (event) => {
     setSelectedAnswer(event.target.value);
   };
 
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const currentQuestion = questions[currentQuestionIndex];
-
+  
     alert(
       selectedAnswer === currentQuestion.correctAnswer ? 'Correct!' : 'Incorrect... Try again'
     );
-
+  
     setSelectedAnswer('');
+  
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }else {
+      console.log(score)
+    }
+     
+  };
+  
+  
+  const handleFinalButton = () => {
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      setScore(score + 1);
+    }
+  
+    if (currentQuestionIndex === 9) {
+      setIsOver(true);
+    }
+  };
+  
 
   
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
 
   const handlePreviousQuestion = () => {
     setCurrentQuestionIndex(currentQuestionIndex - 1);
   };
 
-  
 
+  
   const currentQuestion = questions[currentQuestionIndex];
+
 
   return (
     <div className="mainContainerSurvey">
@@ -75,12 +98,29 @@ function Survey() {
 
 {currentQuestionIndex === 9
 ?
-<button type="submit">
+<button 
+type="submit"
+onClick={handleFinalButton}>
 Finish
 </button>
 : console.log(currentQuestionIndex)}
+
 </div>
           </form>
+          {isOver && (
+      <div>
+        <h2>{`You answered ${score} questions correctly!`}</h2>
+        <button
+          type="button"
+          onClick={() => {
+            setIsOver(false);
+            setCurrentQuestionIndex(0);
+          }}
+        >
+          Try Again
+        </button>
+      </div>
+    )}
           <p>Made by Jose Arias</p>
         </div>
         
